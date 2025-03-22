d164fd3ee601be098a6a94dff52c66d8b1692fdd
Rohit yadav <71065155+Rohit-Yadav-47@users.noreply.github.com>
2025-03-22 23:12:41 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -70,5 +70,5 @@ async def proxy_to_vite(path: str, request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("API_PORT", "3001"))
+    port = int(os.getenv("API_PORT", "3000"))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
