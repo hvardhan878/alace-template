42feaf202885c0135a2f967dba97afc219ce7eef
Rohit Yadav <computerfun200@gmail.com>
2025-05-25 18:04:01 +0530

    build

--- server/index.ts
+++ server/index.ts
@@ -50,11 +50,9 @@ app.use((req, res, next) => {
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
-    await setupVite(app, server);
-  } else {
-    serveStatic(app);
-  }
+  
+  // Serve the built production app
+  serveStatic(app);
 
   // ALWAYS serve the app on port 4000
   // this serves both the API and the client.
